7c43a8df773952b263d62a94b3969f1c6b1d7f8d
yuvarajmanivannan <yuvarajm.22aid@kongu.edu>
2024-11-06 18:29:44 +0530

    creditcard_fraud_detection

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import numpy as np
+
+# Load the model
+model = joblib.load(r'C:\Users\YUVARAJ\PycharmProjects\creditcard_fraud_detection\credit_card_fraud_model.joblib')
+
+app = Flask(__name__)
+
+# Home route to serve the HTML page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array([
+        data['Time'], data['V1'], data['V2'], data['V3'], data['V4'], data['V5'],
+        data['V6'], data['V7'], data['V8'], data['V9'], data['V10'], data['V11'],
+        data['V12'], data['V13'], data['V14'], data['V15'], data['V16'], data['V17'],
+        data['V18'], data['V19'], data['V20'], data['V21'], data['V22'], data['V23'],
+        data['V24'], data['V25'], data['V26'], data['V27'], data['V28'], data['Amount']
+    ])
+    prediction = model.predict([features])
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
